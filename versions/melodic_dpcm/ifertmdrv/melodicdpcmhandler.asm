8396814127f79f5b520f4ea7d0b8f5a67a3b6c8f
MAHBOD-85 <backslashvortex@gmail.com>
2025-09-08 18:35:05 +0330

    fix dpcm handler (dammit)

--- versions/melodic_dpcm/ifertmdrv/melodicdpcmhandler.asm
+++ versions/melodic_dpcm/ifertmdrv/melodicdpcmhandler.asm
@@ -1,13 +1,13 @@
   LDA DPCMready ; DPCM BUFFER HANDLER
   BEQ ignoredpcm
-  AND #$0F
-  ORA dpcmtbl,x
-  STA $4010
-  LDA DPCMready
   AND #$F0
   LSR a
   LSR a
   TAX
+  LDA DPCMready
+  AND #$0F
+  ORA dpcmtbl,x
+  STA $4010
   LDA dpcmtbl+1,x
   STA $4011
   LDA dpcmtbl+2,x
